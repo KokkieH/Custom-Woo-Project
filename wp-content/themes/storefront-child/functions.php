42a3ead1b11c3c478c5f940f37cd7948d58e9347
KokkieH <KokkieH@users.noreply.github.com>
2021-12-13 18:33:32 +0200

    Update to child theme to fetch commit data from GitHud and display on product page

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -20,11 +20,28 @@ if ( ! function_exists( 'storefront_credit' ) ) {
 	}
 }
 
-function kokkie_product_summary() {
-    ?>
-	<div>Hello world</div>
-	<?php
+function commit_product_summary() {
+
+	$commit_url = "https://api.github.com/repos/KokkieH/Advent-of-Code-2021/commits/eb1d6e113014a1c6fbfc0c4b8e7420b29d98d131";
+	$commit_request = wp_remote_get( $commit_url );
+	if ( is_wp_error( $commit_request ) ) {
+		return false;
+	}
+
+	$commit_body = wp_remote_retrieve_body( $commit_request );
+
+	$commit_data = json_decode( $commit_body );
+	
+	if ( ! empty( $commit_data ) ) {
+		echo '<h3>Commit hash</h3>';
+		echo '<a href="' . $commit_data->html_url . '">' . substr($commit_data->sha, 0, 7) . '</a>';
+		echo '<h3>Commit Author</h3>';
+		echo '<a href="' . $commit_data->author->html_url . '">' . $commit_data->commit->author->name . '</a>';
+		echo '<h3>Commit message</h3>';
+		echo  $commit_data->commit->message;
+	}
 }
-add_action( 'woocommerce_single_product_summary', 'kokkie_product_summary' );
+
+add_action( 'woocommerce_single_product_summary', 'commit_product_summary' );
 
 ?>
